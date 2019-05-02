e3ee92b7e8c308a470bcb0f0c815cc9f095d6f74
Pavel Aleksandrov <inblank@yandex.ru>
2019-05-02 23:00:17 +0300

    In ActiveRecord, the search for a database connection for a module is added.

--- src/ActiveRecord.php
+++ src/ActiveRecord.php
@@ -2,6 +2,7 @@
 
 namespace inblank\dbhelper;
 
+use Yii;
 use yii\helpers\Inflector;
 use yii\helpers\StringHelper;
 
@@ -11,7 +12,32 @@ use yii\helpers\StringHelper;
 class ActiveRecord extends \yii\db\ActiveRecord
 {
     /**
+     * Getting a connection to the database considering the module settings
+     * @return \yii\db\Connection the database connection used by this AR class.
+     * @throws \yii\base\InvalidConfigException
+     */
+    public static function getDb()
+    {
+        static $db;
+        if (is_null($db)) {
+            $class = StringHelper::dirname(StringHelper::dirname(static::class)) . '\Module';
+            foreach (Yii::$app->getModules() as $module) {
+                if ($module instanceof $class) {
+                    $db = is_string($module->db) ? Yii::$app->get($module->db) : $module->db;
+                    break;
+                }
+            }
+            if (is_null($db)) {
+                // did not find the module, using the default connection
+                $db = Yii::$app->get('db');
+            }
+        }
+        return $db;
+    }
+
+    /**
      * {@inheritDoc}
+     * @throws \yii\base\InvalidConfigException
      */
     public static function tableName()
     {
